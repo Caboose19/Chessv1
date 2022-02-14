e1467d809557843a88f5e123e6cdb29898317279
Caboose19 <zachrules3@gmail.com>
2022-02-14 13:44:26 -0700

    Update Game.cs

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -20,6 +20,14 @@ public class Game : MonoBehaviour
     private GameObject[] playerBlack = new GameObject[16];
     private GameObject[] playerWhite = new GameObject[16];
 
+    private int pos = 0;
+    private int pos1 = 1;
+    private int pos2 = 2;
+    private int pos3 = 3; 
+    private int pos4 = 4;
+    private int pos5 = 5;
+    private int pos6 = 6;
+    private int pos7 = 7;
 
     //current turn
     private string currentPlayer = "white";
@@ -57,50 +65,139 @@ public class Game : MonoBehaviour
 
     public void SpecialGame()
     {
-        int pos = Random.Range(0, 8);
-        int pos1 = Random.Range(0, 8);
-        int pos2 = Random.Range(0, 8);
-        int pos3 = Random.Range(0, 8);
-        int pos4 = Random.Range(0, 8);
-        int pos5 = Random.Range(0, 8);
-        int pos6 = Random.Range(0, 8);
-        int pos7 = Random.Range(0, 8);
-        //Checks that pos doesn't overlap ever but still reamins as random so we have no overlap not the prettiest solution but workable
-        /*
-                while (pos == pos1 || pos == pos2 || pos == pos3 || pos == pos4 || pos == pos5 || pos == pos6 || pos == pos7)
-                {
-                    pos = Random.Range(0, 7);
-                }*/
-        while (pos == pos1 || pos2 == pos1 || pos3 == pos1 || pos4 == pos1 || pos5 == pos1 || pos6 == pos1 || pos7 == pos1)
+        //rooks
+        pos = Random.Range(0, 8);
+        pos7 = Random.Range(0, 8);
+        //Knights
+        pos1 = Random.Range(0, 8);
+        pos6 = Random.Range(0, 8);
+
+        //Bishops
+        pos2 = Random.Range(0, 8);
+        if (pos2 % 2 == 0)
         {
-            pos1 = Random.Range(0, 7);
+            pos5 = Random.Range(1, 7);
         }
+
+        //Queen
+        pos3 = Random.Range(0, 8);
+
+        //King
+        if (pos > pos7)
+        {
+            pos4 = Random.Range(pos7, pos -1);
+        }
+        else
+        {
+            pos4 = Random.Range(pos, pos7 -1);
+        }
+        //Checks that pos doesn't overlap ever but still reamins as random so we have no overlap not the prettiest solution but workable
+
+        //rooks
+        while (pos == pos1 || pos2 == pos || pos3 == pos || pos4 == pos || pos5 == pos || pos6 == pos || pos7 == pos)
+        {
+            pos = Random.Range(0, 8);
+            if (pos - 1 == pos7 || pos == pos7 - 1)
+            {
+                pos = Random.Range(0, 8);
+                pos7 = Random.Range(0, 8);
+            }
+        }
+        while (pos == pos7 || pos1 == pos7 || pos2 == pos7 || pos3 == pos7 || pos4 == pos7 || pos5 == pos7 || pos6 == pos7)
+        {
+            pos7 = Random.Range(0, 8);
+            if (pos - 1 == pos7 || pos == pos7 - 1)
+            {
+                pos7 = Random.Range(0, 8);
+                pos = Random.Range(0, 8);
+            }
+        }
+        //king 
+        while (pos == pos4 || pos1 == pos4 || pos2 == pos4 || pos3 == pos4 || pos5 == pos4 || pos6 == pos4 || pos7 == pos4)
+        {
+            if (pos > pos7)
+            {
+                while (pos4 > pos && pos4 < pos7)
+                {
+                    pos4 = Random.Range(pos7, pos - 1);
+                    break;
+                }
+            }
+            else if (pos7 > pos)
+            {
+                while (pos4 < pos && pos4 > pos7)
+                {
+                    pos4 = Random.Range(pos, pos7 - 1);
+                    break;
+                }
+
+            }
+
+        }
+
         while (pos == pos2 || pos1 == pos2 || pos3 == pos2 || pos4 == pos2 || pos5 == pos2 || pos6 == pos2 || pos7 == pos2)
         {
             pos2 = Random.Range(0, 7);
+
+            while (pos2 % 2 == 0)
+            {
+                pos5 = Random.Range(1, 7);
+            }
+            while (pos2 % 2 != 0)
+            {
+                pos5 = Random.Range(0, 8);
+            }
         }
+
         while (pos == pos3 || pos1 == pos3 || pos2 == pos3 || pos4 == pos3 || pos5 == pos3 || pos6 == pos3 || pos7 == pos3)
         {
             pos3 = Random.Range(0, 7);
-        }
-        while (pos == pos4 || pos1 == pos4 || pos2 == pos4 || pos3 == pos4 || pos5 == pos4 || pos6 == pos4 || pos7 == pos4)
-        {
-            pos4 = Random.Range(0, 7);
-        }
-        while (pos == pos5 || pos1 == pos5 || pos2 == pos5 || pos3 == pos5 || pos5 == pos4 || pos6 == pos5 || pos7 == pos5)
-        {
-            pos5 = Random.Range(0, 7);
         }
         while (pos == pos6 || pos1 == pos6 || pos2 == pos6 || pos3 == pos6 || pos4 == pos6 || pos5 == pos6 || pos7 == pos6)
         {
             pos6 = Random.Range(0, 7);
         }
-        while (pos == pos7 || pos1 == pos7 || pos2 == pos7 || pos3 == pos7 || pos4 == pos7 || pos5 == pos7 || pos6 == pos7)
-        {
-            pos7 = Random.Range(0, 7);
-        }
+
+        string test = pos + " " + pos1 + " " + pos2 + " " + pos3 + " " + pos4 + " " + pos5 + " " + pos6 + " " + pos7;
+
+
+        /* return test;*/
+
+        Debug.Log(test);
+
+
 
         //Place pos on the board using random x coord
+
+        playerWhite = new GameObject[] { Create("white_rook", pos, 0), /*Create("white_knight", pos1, 0),*/
+            Create("white_bishop", pos2, 0),/* Create("white_queen", pos3, 0),*/ Create("white_king", pos4, 0),
+            Create("white_bishop", pos5, 0), Create("white_knight", pos6, 0), Create("white_rook", pos7, 0),
+           /* Create("white_pawn", 0, 1), Create("white_pawn", 1, 1), Create("white_pawn", 2, 1),
+            Create("white_pawn", 3, 1), Create("white_pawn", 4, 1), Create("white_pawn", 5, 1),
+            Create("white_pawn", 6, 1), Create("white_pawn", 7, 1)*/ };
+        playerBlack = new GameObject[] { Create("black_rook", pos, 7), /*Create("black_knight",pos1,7),*/
+            Create("black_bishop",pos2,7), /*Create("black_queen",pos3,7),*/ Create("black_king",pos4,7),
+            Create("black_bishop",pos5,7), /*Create("black_knight",pos6,7),*/ Create("black_rook",pos7,7)
+            /*Create("black_pawn", 0, 6), Create("black_pawn", 1, 6), Create("black_pawn", 2, 6),
+            Create("black_pawn", 3, 6), Create("black_pawn", 4, 6), Create("black_pawn", 5, 6),
+            Create("black_pawn", 6, 6), Create("black_pawn", 7, 6)*/ };
+
+        //Set all piece positions on the positions board
+        for (int i = 0; i < playerWhite.Length; i++)
+        {
+            SetPosition(playerBlack[i]);
+            SetPosition(playerWhite[i]);
+        }
+        for (int i = 0; i < playerBlack.Length-8; i++)
+        {
+            string placeTest = playerBlack[i].name + " " + playerBlack[i].transform.position.x + " " + playerWhite[i].name + " " + playerWhite[i].transform.position.x;
+
+            Debug.Log(placeTest);
+        }
+    }
+
+    public void NormalGame()
+    {
 
         playerWhite = new GameObject[] { Create("white_rook", pos, 0), Create("white_knight", pos1, 0),
             Create("white_bishop", pos2, 0), Create("white_queen", pos3, 0), Create("white_king", pos4, 0),
@@ -108,6 +205,7 @@ public class Game : MonoBehaviour
             Create("white_pawn", 0, 1), Create("white_pawn", 1, 1), Create("white_pawn", 2, 1),
             Create("white_pawn", 3, 1), Create("white_pawn", 4, 1), Create("white_pawn", 5, 1),
             Create("white_pawn", 6, 1), Create("white_pawn", 7, 1) };
+
         playerBlack = new GameObject[] { Create("black_rook", pos, 7), Create("black_knight",pos1,7),
             Create("black_bishop",pos2,7), Create("black_queen",pos3,7), Create("black_king",pos4,7),
             Create("black_bishop",pos5,7), Create("black_knight",pos6,7), Create("black_rook",pos7,7),
@@ -121,29 +219,19 @@ public class Game : MonoBehaviour
             SetPosition(playerBlack[i]);
             SetPosition(playerWhite[i]);
         }
-    }
 
-    public void NormalGame()
-    {
+        string test = pos + " " + pos1 + " " + pos2 + " " + pos3 + " " + pos4 + " " + pos5 + " " + pos6 + " " + pos7 + ":Non Random";
 
-        playerWhite = new GameObject[] { Create("white_rook", 0, 0), Create("white_knight", 1, 0),
-            Create("white_bishop", 2, 0), Create("white_queen", 3, 0), Create("white_king", 4, 0),
-            Create("white_bishop", 5, 0), Create("white_knight", 6, 0), Create("white_rook", 7, 0),
-            Create("white_pawn", 0, 1), Create("white_pawn", 1, 1), Create("white_pawn", 2, 1),
-            Create("white_pawn", 3, 1), Create("white_pawn", 4, 1), Create("white_pawn", 5, 1),
-            Create("white_pawn", 6, 1), Create("white_pawn", 7, 1) };
-        playerBlack = new GameObject[] { Create("black_rook", 0, 7), Create("black_knight",1,7),
-            Create("black_bishop",2,7), Create("black_queen",3,7), Create("black_king",4,7),
-            Create("black_bishop",5,7), Create("black_knight",6,7), Create("black_rook",7,7),
-            Create("black_pawn", 0, 6), Create("black_pawn", 1, 6), Create("black_pawn", 2, 6),
-            Create("black_pawn", 3, 6), Create("black_pawn", 4, 6), Create("black_pawn", 5, 6),
-            Create("black_pawn", 6, 6), Create("black_pawn", 7, 6) };
 
-        //Set all piece positions on the positions board
-        for (int i = 0; i < playerWhite.Length; i++)
+        /* return test;*/
+
+        Debug.Log(test);
+
+        for (int i = 0; i < playerBlack.Length - 8; i++)
         {
-            SetPosition(playerBlack[i]);
-            SetPosition(playerWhite[i]);
+            string placeTest = playerBlack[i].name + " " + playerBlack[i].transform.position.x + " " + playerWhite[i].name + " " + playerWhite[i].transform.position.x;
+
+            Debug.Log(placeTest);
         }
     }
 
